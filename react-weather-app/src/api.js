cbc21f7735e9649803323992f1369d85504edd90
samuelvilkovsky <vilkovskysamuel2@gmail.com>
2023-01-25 22:37:51 +0100

    first commit

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '856cdfe7a7msh17233924ce7fcd9p161896jsn36fcc00122ce',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7eff87ca30968b4a885ae7ad8e25cf13";
